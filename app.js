cf3dd962e0cca7d180a34a0e52ef5eb483f96115
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2020-09-24 18:44:18 -0300

    commit

--- app.js
+++ app.js
@@ -2,31 +2,27 @@ const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
 const generatePokemonPromises = () => Array(150).fill().map((_, index) => 
  fetch(getPokemonUrl(index + 1)).then(response => response.json()))
- 
-const fetchPokemon = () => {
 
-   const pokemonPromisses = generatePokemonPromises()
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+    const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
-    Promise.all(pokemonPromisses)
-     .then(pokemons => {
+    accumulator += `
+           <li class="card ${elementTypes[0]}">
+             <img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" />
+             <h2 class="card-title">${id}. ${name}</h2>
+             <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+           </li>
+       `
+    return accumulator
+}, '')
 
-         const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-             const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+const insertPokemonsIntoPage = (pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
+ })
 
-             accumulator += `
-                <li class="card ${types[0]}">
-                  <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
-                  <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                  <p class="card-subtitle">${types.join(' | ')}</p>
-                </li>
-            `
-            return accumulator
-         }, '')
+const pokemonPromisses = generatePokemonPromises()
 
-         const ul = document.querySelector('[data-js="pokedex"]')
-
-        ul.innerHTML = lisPokemons
-     })
-}
-
-fetchPokemon()
+Promise.all(pokemonPromisses)
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)
